b2e3e883b2841b6ef4350e253f327453f9cacc07
Marek Zwick <utahmarek@gmail.com>
2015-12-03 11:12:22 -0800

    DOMNode collection methods written

--- jQuery_lite/js/jquery_lite.js
+++ jQuery_lite/js/jquery_lite.js
@@ -3,17 +3,55 @@
     this.nodeArray = nodeArray;
   }
 
-  DOMNodeCollection.prototype.html = function () {
-    
+  Array.prototype.remove = function(set) {
+    return this.filter(function(e) {
+      return set.indexOf(e) < 0;
+    });
+  };
+
+  DOMNodeCollection.prototype.html = function (string) {
+    if (typeof string === "undefined") {
+      return this.nodeArray[0].innerHTML;
+    }
+
+    for (var i = 0; i < this.nodeArray.length; i++) {
+      this.nodeArray[i].innerHTML = string;
+    }
   };
 
   DOMNodeCollection.prototype.empty = function () {
-
+    for (var i = 0; i < this.nodeArray.length; i++) {
+      this.nodeArray[i].html = "";
+    }
   };
 
-  DOMNodeCollection.prototype.append = function () {
-
+  DOMNodeCollection.prototype.append = function (content) {
+    this.nodeArray.concat(content);
   };
+
+  DOMNodeCollection.prototype.attr = function () {
+    return this.nodeArray[0].attributes;
+  };
+
+  DOMNodeCollection.prototype.addClass = function (className) {
+    for (var i = 0; i < this.nodeArray.length; i++) {
+      if (this.nodeArray[i].className.length === 0) {
+        this.nodeArray[i].className.concat(className);
+      } else {
+        this.nodeArray[i].className.concat(" " + className);
+      }
+    }
+  };
+
+  DOMNodeCollection.prototype.removeClass = function (content) {
+    var removeClasses = content.split(" ");
+    for (var j = 0; j < this.nodeArray.length; j++) {
+      var nodeClasses = this.nodeArray[j].className.split(" ");
+      var newClasses = nodeClasses.remove(removeClasses);
+      this.nodeArray[j].className = newClasses.join(" ");
+    }
+  };
+
 
   window.$l = function (arg) {
     var nodeArray;
