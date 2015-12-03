9ddc13bb38762246e713e6504b62c5610fd32b91
Marek Zwick <utahmarek@gmail.com>
2015-12-03 15:19:17 -0800

    We just tested all of this stuff. Itsucked. I'm lonely. Call me. I soiled myself.

--- jQuery_lite/js/jquery_lite.js
+++ jQuery_lite/js/jquery_lite.js
@@ -27,24 +27,46 @@
 
   DOMNodeCollection.prototype.empty = function () {
     for (var i = 0; i < this.nodeArray.length; i++) {
-      this.nodeArray[i].html = "";
+      this.nodeArray[i].innerHTML = "";
+    }
+  };
+  // htmlString, element, array, jQuery_lite
+  DOMNodeCollection.prototype.append = function (content) {
+    if (typeof content === "string") {
+      var newNode = document.createTextNode(content);
+      for (var i = 0; i < this.nodeArray.length; i++) {
+        this.nodeArray[i].appendChild(newNode);
+      }
+    } else if (content instanceof Array) {
+      for (i = 0; i < this.nodeArray.length; i++) {
+        for (var j = 0; j < content.length; j++) {
+          this.nodeArray[i].appendChild(content[j].cloneNode());
+        }
+      }
+    } else if (content.constructor.name === "DOMNodeCollection") {
+      for (i = 0; i < this.nodeArray.length; i++) {
+        for (j = 0; j < content.nodeArray.length; j++) {
+          this.nodeArray[i].appendChild(content.nodeArray[j].cloneNode());
+        }
+      }
+    } else if (content instanceof HTMLElement) {
+      for (i = 0; i < this.nodeArray.length; i++) {
+        this.nodeArray[i].appendChild(content.cloneNode());
+      }
     }
   };
 
-  DOMNodeCollection.prototype.append = function (content) {
-    this.nodeArray.concat(content);
-  };
-
-  DOMNodeCollection.prototype.attr = function () {
-    return this.nodeArray[0].attributes;
+  DOMNodeCollection.prototype.attr = function (attrName) {
+    return this.nodeArray[0].attributes[attrName].value;
   };
 
   DOMNodeCollection.prototype.addClass = function (className) {
     for (var i = 0; i < this.nodeArray.length; i++) {
       if (this.nodeArray[i].className.length === 0) {
-        this.nodeArray[i].className.concat(className);
+        this.nodeArray[i].className = className;
       } else {
-        this.nodeArray[i].className.concat(" " + className);
+        var oldClass = this.nodeArray[i].className;
+        this.nodeArray[i].className = oldClass + " " + className;
       }
     }
   };
@@ -59,18 +81,20 @@
   };
 
   DOMNodeCollection.prototype.children = function () {
-    var childNodes = [];
+    var nodeArray = [];
     for (var i = 0; i < this.nodeArray.length; i++) {
-      childNodes.concat(this.nodeArray[i].children);
+      var childList = this.nodeArray[i].children;
+      var childNodes = [].slice.call(childList);
+      nodeArray = nodeArray.concat(childNodes);
     }
-    return new DOMNodeCollection(childNodes);
+    return new DOMNodeCollection(nodeArray);
   };
 
   DOMNodeCollection.prototype.parent = function () {
     var parentNodes = [];
     for (var i = 0; i < this.nodeArray.length; i++) {
       if (this.nodeArray[i].parentElement) {
-        parentNodes.concat(this.nodeArray[i].parentElement);
+        parentNodes = parentNodes.concat(this.nodeArray[i].parentElement);
       }
     }
     return new DOMNodeCollection(parentNodes);
@@ -88,7 +112,7 @@
 
   DOMNodeCollection.prototype.remove = function () {
     for (var i = 0; i < this.nodeArray.length; i++) {
-      this.nodeArray[i].parent.removeChild(this.nodeArray[i]);
+      this.nodeArray[i].parentNode.removeChild(this.nodeArray[i]);
     }
     this.nodeArray = [];
   };
@@ -96,13 +120,13 @@
 
   window.$l = function (arg) {
     var nodeArray;
-    if (typeof arg === "string") {
+    if (arg instanceof HTMLElement) {
+      nodeArray = [arg];
+    }
+    else if (typeof arg === "string") {
       var nodeList = document.querySelectorAll(arg);
       nodeArray = [].slice.call(nodeList);
-    } else if (arg instanceof HTMLElement) {
-      nodeArray = [arg];
     }
     return new DOMNodeCollection(nodeArray);
   };
-
 })();
